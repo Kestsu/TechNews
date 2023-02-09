c3f78b42e2a506b12a7e63a344bdeb748d59bdf7
Kevin Li <Kevinstart14@gmail.com>
2023-02-09 19:35:33 -0300

    feito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -30,7 +30,31 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    url_schema = "head > link[rel = canonical] ::attr(href)"
+    url = Selector(html_content).css(url_schema).get()
+    title = Selector(html_content).css("div > h1.entry-title ::text").get()
+    timestamp = Selector(html_content).css("ul > li.meta-date ::text").get()
+    writer_schema = "li.meta-author > span.author > a ::text"
+    writer = Selector(html_content).css(writer_schema).get()
+    #  Vai pegar so o primeiro elemento
+    time_schema = "li.meta-reading-time ::text"
+    time = Selector(html_content).css(time_schema).get().split()[0]
+    summary_schema = "div.entry-content > p:nth-child(1) ::text"
+    summary = ''.join(Selector(html_content).css(summary_schema).getall())
+    if summary == '':
+        summary_schema = "div.entry-content > p:nth-child(2) ::text"
+        summary = ''.join(Selector(html_content).css(summary_schema).getall())
+    category_sch = 'div.meta-category > a.category-style > span.label ::text'
+    category = Selector(html_content).css(category_sch).get()
+    return {
+        "url": url,
+        "title": title.strip(),  # remover espaço do final
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": int(time),
+        "summary": summary.strip(),
+        "category": category,
+    }
 
 
 # Requisito 5
