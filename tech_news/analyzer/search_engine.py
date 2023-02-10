3554cdeea1416d4bdfce81a4312cb323ff591376
Kevin Li <Kevinstart14@gmail.com>
2023-02-10 14:44:32 -0300

    feito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,13 +1,42 @@
+from tech_news.database import search_news
+from tech_news.scraper import get_tech_news
+# import datetime
+
+
 # Requisito 7
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+def search_by_title(title):  # N sei pq ta falhando
+    list_all = search_news({"title": {"$regex": title.lower()}})
+    list_tuple = []
+    for dic in list_all:
+        list_tuple.append((dic["title"], dic["url"]))
+
+    return list_tuple
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:  # n ta funcionando a query
+        # data_obj = datetime.datetime.strptime(date, '%Y-%m-%d')
+        # date_formatted = data_obj.date()
+        list_all = search_news({"timestamp": {f"$eq: new Date({date})"}})
+
+        return list_all
+        # list_tuple = []
+        # for dic in list_all:
+        # list_tuple.append((dic["title"], dic["url"]))
+        # return list_tuple
+    except ValueError:
+        return "Data inválida"
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    list_all = search_news({"category": category})
+    list_tuple = []
+    for dic in list_all:
+        list_tuple.append((dic["title"], dic["url"]))
+    return list_tuple
+
+
+def popular_banco(date):
+    get_tech_news(date)
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -65,17 +65,59 @@ def get_tech_news(amount):
     pages_need = 1
     links = []
     info_pages = []
+    # Vai calcular quantas paginas serão necessarias
     if amount > 12:
         pages_need = math.ceil(amount / 12)
+    #  Vai acessas todas as paginas e salvar todos os links em uma so lista
     for quantity in range(pages_need):
         links.extend(scrape_updates(res))
         res = fetch(scrape_next_page_link(res))
+    #  Vai entrar em todos os links, pegas as info e depois salvar na lista
     for url in links:
         info_pages.append(scrape_news(fetch(url)))
+    #  Vai salvar no banco so a quantidade pedida
     create_news(info_pages[:amount])
+    #  Vai retornar so a quantidade que pediu
     return info_pages[:amount]
 
 
-if __name__ == "__main__":
-    amount = input("Digite um numero: ")
-    get_tech_news(int(amount))
+# def get_tech(amount):
+#     quantify_notices = [amount]
+#     primary_page = fetch('https://blog.betrybe.com/')
+#     # next_page = scrape_next_page_link(primary_page)
+#     # updates = scrape_updates(fetch(next_page))
+#     # print(next_page)
+#     notices_list = []
+#     quantify = 0
+#     for notices in quantify_notices:
+#         if quantify_notices[0] > quantify:
+#             final_count = 0
+#             final_count += 1
+#             if final_count == 1:
+#                 notices_length = scrape_updates(primary_page)
+#                 notices_list += notices_length
+#                 quantify += len(notices_length)
+#             pages = scrape_next_page_link(
+#                 fetch(f"""https://blog.betrybe.com/page/{notices + 1}/"""))
+#             notices_length = scrape_updates(fetch(pages))
+#             notices_list += notices_length
+#             quantify += len(notices_length)
+#             # print('a', quantify)
+#     list_links = notices_list[:quantify_notices[0]]
+#     list_dict = []
+#     for links in list_links:
+#         list_dict.append(scrape_news(fetch(links)))
+#     # print(list_dict)
+#     # create_news(list_dict)
+#     # print(len(list_dict))
+#     return list_dict
+
+
+# if __name__ == '__main__':
+#     a = get_tech(20)
+#     b = get_tech_news(20)
+
+
+# if __name__ == "__main__":
+#     amount = input("Digite um numero: ")
+#     get_tech_news(int(amount))
