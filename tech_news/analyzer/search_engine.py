78c97dcd1f3e4c9a69e508e3f2c224f6ab3872bc
Kevin Li <Kevinstart14@gmail.com>
2023-02-14 03:56:22 -0300

    feito a data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -23,34 +23,24 @@ def search_by_title(title):  # N sei pq ta falhando
 
 
 # Requisito 8
+
+
 def search_by_date(data):
     try:
-        dia, mes, ano = data.split("-")
-        # print(len(dia), mes, ano)
-        diaMaior = int(dia) > 2
-        mesMaior = int(mes) > 12
-        if len(dia) == 2 & len(mes) == 2:
-            if diaMaior > mesMaior:
-                list_all = search_news({"timestamp": data.replace('-', '/')})
-            elif diaMaior < mesMaior:
-                list_all = search_news({"timestamp": data.replace('-', '/')})
-            else:
-                raise ValueError("Data inválida")
-        else:
-            data_formatada = datetime.strptime(
-                data, "%Y-%m-%d").strftime("%d/%m/%Y")
-            list_all = search_news({"timestamp": data_formatada})
+        data_formatada = datetime.strptime(
+            data, "%Y-%m-%d").strftime("%d/%m/%Y")
+        list_all = search_news({"timestamp": data_formatada})
         list_tuple = []
         for dic in list_all:
             list_tuple.append((dic["title"], dic["url"]))
         return list_tuple
     except ValueError:
-        raise TypeError("Data inválida")
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
 def search_by_category(category):
-    # print(category.lower().title())
+
     list_all = search_news({"category": category.lower().title()})
     list_tuple = []
     for dic in list_all:
